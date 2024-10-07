1f3674e780360b2f23af44826a5e3ceb231ce82f
Jesper Falkenby <jesper.hansson.f@gmail.com>
2024-10-07 16:41:56 +0200

    Improve Mac build script, allow setting qt root

--- clean_build.sh
+++ clean_build.sh
@@ -11,21 +11,27 @@ if [ -z "$B_CMAKE" ]; then
     exit 1
 fi
 
+# Allow local customizations to build environment
+[ -r ./build_env.sh ] && . ./build_env.sh
+
 B_BUILD_DIR="${B_BUILD_DIR:-build}"
 B_BUILD_TYPE="${B_BUILD_TYPE:-Debug}"
 B_CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=${B_BUILD_TYPE} ${B_CMAKE_FLAGS:-}"
 
+# Allow setting custom QT root
+if ! [ -z "$B_QT_ROOT" ]; then
+    echo "Using QT root: ${B_QT_ROOT}"
+    B_CMAKE_FLAGS="${B_CMAKE_FLAGS} -DCMAKE_PREFIX_PATH=${B_QT_ROOT}"
+fi
+
 if [ "$(uname)" = "Darwin" ]; then
-    B_CMAKE_FLAGS="${B_CMAKE_FLAGS} -DCMAKE_OSX_SYSROOT=$(xcrun --sdk macosx --show-sdk-path) -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15"
+    B_CMAKE_FLAGS="${B_CMAKE_FLAGS} -DCMAKE_OSX_SYSROOT=$(xcrun --sdk macosx --show-sdk-path) -DCMAKE_OSX_DEPLOYMENT_TARGET=${B_OSX_DEPLOYMENT_TARGET:-10.15}"
 fi
 
 # Prefer ninja if available
 if command -v ninja 2>/dev/null; then
     B_CMAKE_FLAGS="-GNinja ${B_CMAKE_FLAGS}"
 fi
-
-# allow local customizations to build environment
-[ -r ./build_env.sh ] && . ./build_env.sh
 
 set -e
 
